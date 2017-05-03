7c4b904341fe8c144e0014e075775238d816134e
zwxue <zwxue@talend.com>
2017-05-03 15:32:09 +0800

    fix(TUP-17583): The default java version is 1.7 for new created remote (#1003)
    project.

--- main/plugins/org.talend.migrationTool/src/org/talend/migrationtool/MigrationToolService.java
+++ main/plugins/org.talend.migrationTool/src/org/talend/migrationtool/MigrationToolService.java
@@ -168,8 +168,7 @@ public class MigrationToolService implements IMigrationToolService {
         final List<MigrationTask> done = new ArrayList<MigrationTask>(project.getEmfProject().getMigrationTask());
 
         if (beforeLogon) {
-            boolean isNewProject = project.getEmfProject().getProductVersion() == null ? true : false;
-            setMigrationOnNewProject(beforeLogon && isNewProject && done.isEmpty());
+            setMigrationOnNewProject(done.isEmpty());
         }
         sortMigrationTasks(toExecute);
 
