55fb81642b9f7324bfbfac79a8670fd2c2ef0384
hwang-talend <hwang@talend.com>
2017-06-06 15:11:14 +0800

    fix(TUP-17328):Commandline is not able to get new jars in the lib folder (#1064)
    Conflicts:
    	main/plugins/org.talend.librariesmanager/src/main/java/org/talend/librariesmanager/model/service/AbstractLibrariesService.java

--- main/plugins/org.talend.librariesmanager/src/main/java/org/talend/librariesmanager/model/service/AbstractLibrariesService.java
+++ main/plugins/org.talend.librariesmanager/src/main/java/org/talend/librariesmanager/model/service/AbstractLibrariesService.java
@@ -109,6 +109,13 @@ public abstract class AbstractLibrariesService implements ILibrariesService {
 
         if (!repositoryBundleService.contains(source.getFile())) {
             repositoryBundleService.deploy(sourceFile.toURI());
+            if (PluginChecker.isSVNProviderPluginLoaded()) {
+                ISVNProviderServiceInCoreRuntime svnService = (ISVNProviderServiceInCoreRuntime) GlobalServiceRegister
+                        .getDefault().getService(ISVNProviderServiceInCoreRuntime.class);
+                if (svnService != null && svnService.isSvnLibSetupOnTAC()) {
+                    svnService.syncLibs(null);
+                }
+            }
         }
 
         ModulesNeededProvider.userAddImportModules(targetFile.getPath(), sourceFile.getName(), ELibraryInstallStatus.INSTALLED);
