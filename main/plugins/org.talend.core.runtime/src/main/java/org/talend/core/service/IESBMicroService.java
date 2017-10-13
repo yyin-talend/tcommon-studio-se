dd394c19721cbe1720110eb71a1254eb4f9f02b2
SunChaoqun <csun@talend.com>
2017-10-13 04:58:20 -0500

    TESB-20466:Data Services (REST) as Microservice - Part II (#1246)

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/IESBMicroService.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/IESBMicroService.java
@@ -24,10 +24,13 @@ package org.talend.core.service;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.talend.core.IService;
 import org.talend.core.model.process.IProcess;
+import org.talend.core.model.properties.ProcessItem;
 import org.talend.core.model.properties.Property;
+import org.talend.core.runtime.process.IBuildJobHandler;
 import org.talend.designer.runprocess.IProcessor;
 import org.talend.repository.model.IRepositoryNode;
 
@@ -44,4 +47,29 @@ public interface IESBMicroService extends IService {
 
     IRunnableWithProgress createRunnableWithProgress(Map exportChoiceMap,
             List<? extends IRepositoryNode> nodes, String version, String destinationPath, String context);
+
+    /**
+     * DOC sunchaoqun Comment method "buildJob".
+     * 
+     * @param destinationPath
+     * @param itemToExport
+     * @param version
+     * @param context
+     * @param exportChoiceMap
+     * @param monitor
+     * @throws Exception
+     */
+    void buildJob(String destinationPath, ProcessItem itemToExport, String version, String context, Map exportChoiceMap,
+            IProgressMonitor monitor) throws Exception;
+
+    /**
+     * DOC sunchaoqun Comment method "createBuildJobHandler".
+     * 
+     * @param itemToExport
+     * @param version
+     * @param context
+     * @param exportChoiceMap
+     * @return
+     */
+    IBuildJobHandler createBuildJobHandler(ProcessItem itemToExport, String version, String context, Map exportChoiceMap);
 }
