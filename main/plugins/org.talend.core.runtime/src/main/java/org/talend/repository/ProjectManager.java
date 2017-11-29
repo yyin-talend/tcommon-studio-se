9023ca96febade70a8a062c770e6c01d415f67cf
kjwang-talend <kjwang@talend.com>
2017-11-29 18:29:36 +0800

    TUP-18626 Handle Jobserver authentification (6.5) (#1302)
    https://jira.talendforge.org/browse/TUP-18626

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/repository/ProjectManager.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/repository/ProjectManager.java
@@ -78,6 +78,8 @@ public final class ProjectManager {
     private Project currentProject;
 
     private Map<String, String> mapProjectUrlToBranchUrl = new HashMap<String, String>();
+    
+	private static final Map<String, Integer> projectLabelToIdMap = new HashMap<String, Integer>();
 
     private Map<String, List<FolderItem>> foldersMap = new HashMap<String, List<FolderItem>>();
     
@@ -950,5 +952,20 @@ public final class ProjectManager {
         }
 
         return list;
-    }    
+    } 
+
+	public static void clearCachedProjectIds() {
+		projectLabelToIdMap.clear();
+	}
+
+	public static void cacheProjectId(String projectLabel, Integer projectId) {
+		projectLabelToIdMap.put(projectLabel, projectId);
+	}
+
+	public static String getCachedProjectId(String projectLabel) {
+		if (projectLabelToIdMap.get(projectLabel) != null) {
+			return String.valueOf(projectLabelToIdMap.get(projectLabel));
+		}
+		return null;
+	}
 }
