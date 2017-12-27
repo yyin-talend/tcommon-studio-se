64420f7b37882152d14c0f2a73887c7fa0378427
GGu <ggu@talend.com>
2017-12-27 15:51:19 +0800

    fix(TUP-19089): add the command to command stack to execute (#1342)

--- main/plugins/org.talend.core.ui/src/main/java/org/talend/core/ui/context/SelectRepositoryContextDialog.java
+++ main/plugins/org.talend.core.ui/src/main/java/org/talend/core/ui/context/SelectRepositoryContextDialog.java
@@ -22,6 +22,7 @@ import java.util.Set;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.gef.commands.Command;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
@@ -476,16 +477,7 @@ public class SelectRepositoryContextDialog extends SelectionDialog {
                 // modelManager,
                 // helper, paramTypeList);
 
-                if (modelManager.getCommandStack() != null) {
-
-                    modelManager.getCommandStack().execute(addGroupCmd);
-
-                    // modelManager.getCommandStack().execute(addVarCmd);
-
-                } else {
-                    addGroupCmd.execute();
-                    // addVarCmd.execute();
-                }
+                execCommand(addGroupCmd);
 
                 monitor.done();
 
@@ -498,6 +490,14 @@ public class SelectRepositoryContextDialog extends SelectionDialog {
             MessageBoxExceptionHandler.process(e.getTargetException(), getParentShell());
         } catch (InterruptedException e) {
             // Nothing to do
+        }
+    }
+
+    private void execCommand(Command cmd) {
+        if (modelManager.getCommandStack() != null) {
+            modelManager.getCommandStack().execute(cmd);
+        } else {
+            cmd.execute();
         }
     }
 
@@ -515,19 +515,20 @@ public class SelectRepositoryContextDialog extends SelectionDialog {
             }
             progressDialog(selectedItems, contextGoupNameSet, getSelectedContextParameterType());
 
-            modelManager.refresh();
-        }else{
+        } else {
             IContext defaultContext = manager.getDefaultContext();
             List<IContextParameter> existParas = new ArrayList<>(defaultContext.getContextParameterList());
-          //remove the params which is unchecked
-            for(IContextParameter param : existParas){
-                if (param.isBuiltIn()){
+            // remove the params which is unchecked
+            for (IContextParameter param : existParas) {
+                if (param.isBuiltIn()) {
                     continue;
                 }
-                new ContextRemoveParameterCommand(manager, param.getName(), param.getSource())
-                .execute();
+                ContextRemoveParameterCommand cmd = new ContextRemoveParameterCommand(manager, param.getName(), param.getSource());
+                execCommand(cmd);
             }
         }
+
+        modelManager.refresh();
 
         super.okPressed();
 
