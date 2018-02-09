1e74758e2688064b3e1a3a43830ad2a66470e45c
Irene Wang <iwang@talend.com>
2018-02-09 11:35:01 +0100

    DEVOPS-3791 Update copyright year to 2018

--- test/plugins/org.talend.core.runtime.test/src/org/talend/repository/ReferenceProjectProblemManagerTest.java
+++ test/plugins/org.talend.core.runtime.test/src/org/talend/repository/ReferenceProjectProblemManagerTest.java
@@ -18,7 +18,7 @@ import org.talend.core.model.properties.impl.PropertiesFactoryImpl;
 
 // ============================================================================
 //
-// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
