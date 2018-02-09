eb641e44a045e303546af277201b6fc554d0ae57
Irene Wang <iwang@talend.com>
2018-02-09 14:41:53 +0100

    DEVOPS-3791 Update copyright year to 2018

--- main/plugins/org.talend.librariesmanager/resources/java/routines/system/TraceStatusBean.java
+++ main/plugins/org.talend.librariesmanager/resources/java/routines/system/TraceStatusBean.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 // ============================================================================
 //
-// Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
