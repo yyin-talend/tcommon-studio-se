fb475a324b53d0b38945d0862bdcda649c8d999f
hwang-talend <hwang@talend.com>
2018-02-12 10:03:44 +0800

    fix(TUP-19516):now the driver select table can't be used for jdbc (#1430)
    metedata

--- main/plugins/org.talend.core.ui/src/main/java/org/talend/core/ui/metadata/celleditor/ModuleListCellEditor.java
+++ main/plugins/org.talend.core.ui/src/main/java/org/talend/core/ui/metadata/celleditor/ModuleListCellEditor.java
@@ -205,7 +205,9 @@ public class ModuleListCellEditor extends DialogCellEditor {
         }
         // enable to refresh component setting after change modules.
         IElement element = this.tableParam.getElement();
-        if (element != null && !"cConfig".equals(element.getElementParameter("COMPONENT_NAME").getValue())) {
+        boolean isNotCConfig = element.getElementParameter("COMPONENT_NAME") == null ? 
+                true : !"cConfig".equals(element.getElementParameter("COMPONENT_NAME").getValue());
+        if (element != null && isNotCConfig) {
             IElementParameter updateComponentsParam = element.getElementParameter("UPDATE_COMPONENTS"); //$NON-NLS-1$
             if (updateComponentsParam != null) {
                 updateComponentsParam.setValue(Boolean.TRUE);
