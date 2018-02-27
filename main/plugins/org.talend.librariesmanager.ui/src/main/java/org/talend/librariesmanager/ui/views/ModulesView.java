8cb42052fa1cff5511c66e0eee48d9ed4dcbeb4a
hwang-talend <hwang@talend.com>
2018-02-27 18:35:29 +0800

    fix(TUP-19493):Module View: not possible to download anymore the single (#1434)
    jar

--- main/plugins/org.talend.librariesmanager.ui/src/main/java/org/talend/librariesmanager/ui/views/ModulesView.java
+++ main/plugins/org.talend.librariesmanager.ui/src/main/java/org/talend/librariesmanager/ui/views/ModulesView.java
@@ -27,6 +27,7 @@ import org.talend.core.model.general.ILibrariesService.IChangedLibrariesListener
 import org.talend.core.model.general.ModuleNeeded;
 import org.talend.librariesmanager.ui.LibManagerUiPlugin;
 import org.talend.librariesmanager.ui.actions.CheckModulesAction;
+import org.talend.librariesmanager.ui.actions.DownloadExternalJarAction;
 import org.talend.librariesmanager.ui.actions.ExportCustomSettingsAction;
 import org.talend.librariesmanager.ui.actions.ImportCustomSettingsAction;
 import org.talend.librariesmanager.ui.actions.ImportExternalJarAction;
@@ -132,6 +133,8 @@ public class ModulesView extends ViewPart {
         manager.add(removeAction);
         ImportExternalJarAction importAction = new ImportExternalJarAction();
         manager.add(importAction);
+        DownloadExternalJarAction downloadAcion = new DownloadExternalJarAction();
+        manager.add(downloadAcion);
         return;
     }
 
