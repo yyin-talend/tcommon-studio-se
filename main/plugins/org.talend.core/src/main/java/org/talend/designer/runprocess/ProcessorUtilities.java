7859a203ce1c6bfb90f07f583873d70e5dd26d1e
hwang <hwang@talend.com>
2018-02-28 11:25:28 +0800

    fix(TUP-19526):Dynamic schema with generic JDBCconnection fails when job #1450

--- main/plugins/org.talend.core/src/main/java/org/talend/designer/runprocess/ProcessorUtilities.java
+++ main/plugins/org.talend.core/src/main/java/org/talend/designer/runprocess/ProcessorUtilities.java
@@ -61,6 +61,7 @@ import org.talend.core.model.components.ComponentCategory;
 import org.talend.core.model.general.ModuleNeeded;
 import org.talend.core.model.metadata.IMetadataColumn;
 import org.talend.core.model.metadata.IMetadataTable;
+import org.talend.core.model.process.EParameterFieldType;
 import org.talend.core.model.process.IContext;
 import org.talend.core.model.process.IElementParameter;
 import org.talend.core.model.process.INode;
@@ -613,10 +614,12 @@ public class ProcessorUtilities {
             // to check if node is db component , maybe need modification
             boolean isDbNode = false;
             for (IElementParameter param : (List<? extends IElementParameter>) node.getElementParameters()) {
-                if ("TYPE".equals(param.getName()) && "TEXT".equals(param.getFieldType().getName()) && param.getValue() != null
-                        && !"".equals(param.getValue())) {
-                    isDbNode = true;
-                    break;
+            	if(EParameterFieldType.TEXT == param.getFieldType() && param.getValue() != null){
+                    if(("TYPE".equals(param.getName()) && !"".equals(param.getValue())) || 
+                            ("FAMILY".equals(param.getName()) && param.getValue().toString().startsWith("Database"))){
+                        isDbNode = true;
+                        break;
+                    }
                 }
             }
             if (isDbNode) {
