d76af4ef078567fe39fbabc3f63de6f5c6dc9558
xlwang-tlnd <34297758+xlwang-tlnd@users.noreply.github.com>
2018-03-08 11:33:37 +0800

    Xlwang/tup 19419 import existing valid project into local connection is disabled (#1492)
    * xlwang/fix(TUP-19419)Import Existing 'Valid' project into Local
    Connection is disabled
    
    * xlwang/fix(TUP-19419)_Import_Existing_Valid_project_into_Local_Connection_is_disabled
    
    * xlwang/fix(TUP-19419)_Import_Existing_Valid_project_into_Local_Connection_is_disabled
    
    * xlwang/fix(TUP-19419)_Import_Existing_Valid_project_into_Local_Connection_is_disabled

--- main/plugins/org.talend.utils/src/main/java/org/talend/utils/files/FileUtils.java
+++ main/plugins/org.talend.utils/src/main/java/org/talend/utils/files/FileUtils.java
@@ -25,6 +25,7 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -241,5 +242,35 @@ public final class FileUtils {
         File tmpFolder = new File(System.getProperty("user.dir"), "temp/" + folderName); //$NON-NLS-1$  //$NON-NLS-2$
         tmpFolder.mkdirs();
         return tmpFolder;
+    }
+    /**
+     * DOC xlwang Comment method "createProjectFile".
+     */
+    public static void createProjectFile(String projectName, File tmpProjectFile) throws Exception {
+        tmpProjectFile.createNewFile();
+        FileOutputStream fos = null;
+        PrintStream ps = null;
+        try {
+            fos = new FileOutputStream(tmpProjectFile);
+            ps = new PrintStream(fos);
+            ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            ps.println("<projectDescription>");
+            ps.println("\t<name>" + projectName + "</name>");
+            ps.println("\t<comment></comment>");
+            ps.println("\t<projects></projects>");
+            ps.println("\t<buildSpec></buildSpec>");
+            ps.println("\t<natures>");
+            ps.println("\t\t<nature>org.talend.core.talendnature</nature>");
+            ps.println("\t</natures>");
+            ps.println("</projectDescription>");
+            ps.flush();
+        } finally {
+            if (fos != null) {
+                fos.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+        }
     }
 }
