800fae8bd9d423506c38e3f56f04aaa5a58defe0
Mike Yan <yyan@talend.com>
2018-03-13 15:22:43 +0800

    TESB-21510: Fixed to use osgi assembly to build job as OSGi type

--- main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/creator/CreateMavenStandardJobOSGiPom.java
+++ main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/creator/CreateMavenStandardJobOSGiPom.java
@@ -149,10 +149,10 @@ public class CreateMavenStandardJobOSGiPom extends CreateMavenJobPom {
                 }
 
                 final Map<String, Object> templateParameters = PomUtil.getTemplateParameters(getJobProcessor());
-                String content = MavenTemplateManager.getTemplateContent(templateFile,
-                        IProjectSettingPreferenceConstants.TEMPLATE_ROUTE_ASSEMBLY, JOB_TEMPLATE_BUNDLE,
+                String content = MavenTemplateManager.getTemplateContent(templateFile, null, JOB_TEMPLATE_BUNDLE,
                         IProjectSettingTemplateConstants.PATH_OSGI_BUNDLE + '/'
-                                + IProjectSettingTemplateConstants.ASSEMBLY_JOB_TEMPLATE_FILE_NAME, templateParameters);
+                                + IProjectSettingTemplateConstants.ASSEMBLY_JOB_TEMPLATE_FILE_NAME,
+                        templateParameters);
                 if (content != null) {
                     ByteArrayInputStream source = new ByteArrayInputStream(content.getBytes());
                     if (assemblyFile.exists()) {
