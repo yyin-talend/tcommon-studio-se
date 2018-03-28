e786d3d2426d5b597ab5c402f1b9744d4b4e599c
Jane Ding <35018295+jding-tlnd@users.noreply.github.com>
2018-03-28 13:35:56 +0800

    feat(TUP-19782)Add filter on the pom synchronization (#1560)
    * feat(TUP-19782)Add filter on the pom synchronization
    Add a filter test method
    
    * delete no used import
    
    * Modified wrong artifiactid

--- test/plugins/org.talend.designer.maven.test/src/main/java/org/talend/designer/maven/tools/AggregatorPomsHelperTest.java
+++ test/plugins/org.talend.designer.maven.test/src/main/java/org/talend/designer/maven/tools/AggregatorPomsHelperTest.java
@@ -17,17 +17,29 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+
 import org.apache.maven.model.Model;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.m2e.core.MavenPlugin;
 import org.junit.Test;
+import org.talend.core.model.general.Project;
+import org.talend.core.model.properties.ProcessItem;
 import org.talend.core.model.properties.PropertiesFactory;
 import org.talend.core.model.properties.Property;
+import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.core.runtime.maven.MavenConstants;
+import org.talend.core.runtime.projectsetting.ProjectPreferenceManager;
+import org.talend.designer.core.model.utils.emf.talendfile.ParametersType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+import org.talend.designer.core.model.utils.emf.talendfile.TalendFileFactory;
+import org.talend.designer.maven.DesignerMavenPlugin;
 import org.talend.designer.maven.model.TalendJavaProjectConstants;
+import org.talend.designer.maven.model.TalendMavenConstants;
 import org.talend.repository.ProjectManager;
 
 /**
@@ -46,9 +58,6 @@ public class AggregatorPomsHelperTest {
             jobFolder.create(true, true, null);
         }
         IFile jobPom = jobFolder.getFile("pom.xml");
-        Property property = PropertiesFactory.eINSTANCE.createProperty();
-        property.setLabel("Job1");
-        property.setVersion("1.0");
         AggregatorPomsHelper.addToParentModules(jobPom,null);
 
         IFile projectPom = pomsFolder.getFile("pom.xml");
@@ -61,6 +70,45 @@ public class AggregatorPomsHelperTest {
         model = MavenPlugin.getMavenModelManager().readMavenModel(projectPom);
         assertNotNull(model.getModules());
         assertFalse(model.getModules().contains("jobs/process/job1"));
+    }
+    
+    @Test
+    public void testAddToParentModulesWithFilter() throws Exception {
+    	 String projectTechName = ProjectManager.getInstance().getCurrentProject().getTechnicalLabel();
+         IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+         final IProject project = root.getProject(projectTechName);
+         IFolder pomsFolder = project.getFolder(TalendJavaProjectConstants.DIR_POMS);
+         IFile projectPom = pomsFolder.getFile("pom.xml");
+         Model projectModel = MavenPlugin.getMavenModelManager().readMavenModel(projectPom);
+         
+         IFolder jobFolder = pomsFolder.getFolder("jobs").getFolder("process").getFolder("job1");
+         if (!jobFolder.exists()) {
+             jobFolder.create(true, true, null);
+         }
+         IFile jobPom = jobFolder.getFile("pom.xml");
+         if(!jobPom.exists()) {
+        	 Model jobModel = new Model();
+        	 jobModel.setModelVersion("4.0.0"); 
+        	 jobModel.setGroupId("org.example.aa"); 
+        	 jobModel.setArtifactId(projectModel.getArtifactId());
+        	 jobModel.setVersion(projectModel.getVersion());
+        	 jobModel.setPackaging(TalendMavenConstants.PACKAGING_JAR);
+             MavenPlugin.getMavenModelManager().createMavenModel(jobPom, jobModel);
+         }
+         Property property = createJobProperty("job1","0.1");
+         
+         setFilterContent("(label=other)",projectTechName);
+         AggregatorPomsHelper.addToParentModules(jobPom,property);
+         projectModel = MavenPlugin.getMavenModelManager().readMavenModel(projectPom);
+         assertNotNull(projectModel.getModules());
+         assertFalse(projectModel.getModules().contains("jobs/process/job1"));
+         
+         setFilterContent("(label=job1)",projectTechName);
+         AggregatorPomsHelper.addToParentModules(jobPom,property);
+         projectModel = MavenPlugin.getMavenModelManager().readMavenModel(projectPom);
+         assertNotNull(projectModel.getModules());
+         assertTrue(projectModel.getModules().contains("jobs/process/job1"));
+         
     }
 
     @Test
@@ -88,6 +136,29 @@ public class AggregatorPomsHelperTest {
         String version = "1.0";
         String jobProjectId = AggregatorPomsHelper.getJobProjectId(projectTechName, id, version);
         assertEquals(projectTechName + "|abcde-_e|1.0", jobProjectId);
+    }
+    
+    private Property createJobProperty(String joblabel, String version) throws Exception {
+        Property property = PropertiesFactory.eINSTANCE.createProperty();
+        ProcessItem item = PropertiesFactory.eINSTANCE.createProcessItem();
+        String id = ProxyRepositoryFactory.getInstance().getNextId();
+        property.setId(id);
+        property.setLabel(joblabel);
+        property.setVersion(version);
+        item.setProperty(property);
+        ProcessType process = TalendFileFactory.eINSTANCE.createProcessType();
+        ParametersType parameterType = TalendFileFactory.eINSTANCE.createParametersType();
+        process.setParameters(parameterType);
+        item.setProcess(process);
+        return property;
+
+    }
+    
+    private void setFilterContent(String filter,String projectTechName) {
+    	Project project = ProjectManager.getInstance().getProjectFromProjectTechLabel(projectTechName);
+        ProjectPreferenceManager preferenceManager = new ProjectPreferenceManager(project, DesignerMavenPlugin.PLUGIN_ID,false);
+        IPreferenceStore preferenceStore = preferenceManager.getPreferenceStore();
+        preferenceStore.setValue(MavenConstants.POM_FILTER, filter);
     }
 
 }
