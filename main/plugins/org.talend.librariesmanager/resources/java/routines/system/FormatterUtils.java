d991436b5778de7b83ef1e8b53672c9364ce2342
wang wei <wwang@talend.com>
2018-05-29 14:04:46 +0800

    fix(TDI-39874): Numeric.sequence increments by 2 instead of 1 in tXMLMap (#1690)

--- main/plugins/org.talend.librariesmanager/resources/java/routines/system/FormatterUtils.java
+++ main/plugins/org.talend.librariesmanager/resources/java/routines/system/FormatterUtils.java
@@ -22,6 +22,45 @@ import routines.TalendDate;
 
 public class FormatterUtils {
 
+    /**
+     * format every type object, use runtime type checking
+     * @param obj
+     * @param pattern
+     * @return the string content
+     */
+    public static String fm(Object obj, String pattern) {
+        if(obj == null) {
+            return null;
+        }
+        
+        //the order is on purpose here for the appear rank
+        if(obj instanceof String) {
+            return obj.toString();
+        } else if(obj instanceof Integer) {//not call the format(int i, String pattern) method to avoid auto cast from Integer to int
+            return obj.toString();
+        } else if(obj instanceof Long) {
+            return obj.toString();
+        } else if(obj instanceof Date) {
+            return TalendDate.formatDate(pattern == null ? Constant.dateDefaultPattern : pattern, (Date)obj);
+        } else if(obj instanceof Boolean) {
+          return obj.toString();
+        } else if(obj instanceof BigDecimal) {
+            return ((BigDecimal)obj).toPlainString();
+        } else if(obj instanceof Double) {
+            return obj.toString();
+        } else if(obj instanceof Float) {
+            return obj.toString();
+        } else if(obj instanceof Character) {
+            return obj.toString();
+        } else if(obj instanceof char[]) {
+            return format((char[])obj, null);
+        } else if(obj instanceof byte[]) {
+            return format((byte[])obj, null);
+        }
+        
+        return obj.toString();
+    }
+    
     public static String format(Object obj, String pattern) {
         return (obj == null) ? null : obj.toString();
     }
