115c8b126f22c53c83af7fa8376f3a1710083f5d
hcyi <hcyi@talend.com>
2018-06-04 15:20:53 +0800

    feat(TUP-20033):Audit in project settings in studio. (#1696)
    * feat(TUP-20033):Audit in project settings in studio.
    
    * feat(TUP-20033):improve for Audit in project settings in studio.
    
    * feat(TUP-20033):improve to generate Audit in project settings in studio.
    
    * feat(TUP-20033):improve to generate Audit in project settings in studio.
    
    * feat(TUP-20032):Refactor audit to remove the EMF Query2 dependency.

---
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/ICommandLineService.java
@@ -0,0 +1,29 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.service;
+
+import org.talend.core.IService;
+
+/**
+ * 
+ * created by hcyi on May 10, 2018 Detailled comment
+ *
+ */
+public interface ICommandLineService extends IService{
+
+    public void populateAudit(String url, String driver, String user, String password);
+
+    public void generateAuditReport(String path);
+
+    public void generateAuditReport(String path, String template);
+}
