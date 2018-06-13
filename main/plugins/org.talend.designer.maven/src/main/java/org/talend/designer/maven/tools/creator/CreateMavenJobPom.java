d26ed57d06ccc11080d437b6bb443e7a9f6e18b4
hcyi <hcyi@talend.com>
2018-06-13 10:53:28 +0800

    fix(TUP-20219): Error: java.io.IOException: Split class org.talend.hadoop.mapred.lib.TaggedInputSplit not found. (#1728)
    * fix(TUP-20219): Error: java.io.IOException: Split class
    org.talend.hadoop.mapred.lib.TaggedInputSplit not found.
    
    * fix(TUP-20219): Error: java.io.IOException: Split class
    org.talend.hadoop.mapred.lib.TaggedInputSplit not found.

--- main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/creator/CreateMavenJobPom.java
+++ main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/creator/CreateMavenJobPom.java
@@ -406,9 +406,12 @@ public class CreateMavenJobPom extends AbstractMavenProcessorPom {
 
         StringBuffer windowsScriptAdditionValue = new StringBuffer(50);
         StringBuffer unixScriptAdditionValue = new StringBuffer(50);
-
-        windowsScriptAdditionValue.append(this.getWindowsScriptAddition());
-        unixScriptAdditionValue.append(this.getUnixScriptAddition());
+        if (StringUtils.isNotEmpty(this.getWindowsScriptAddition())) {
+            windowsScriptAdditionValue.append(this.getWindowsScriptAddition());
+        }
+        if (StringUtils.isNotEmpty(this.getWindowsScriptAddition())) {
+            unixScriptAdditionValue.append(this.getUnixScriptAddition());
+        }
 
         // context
         if (isOptionChecked(TalendProcessArgumentConstant.ARG_NEED_CONTEXT)) {
