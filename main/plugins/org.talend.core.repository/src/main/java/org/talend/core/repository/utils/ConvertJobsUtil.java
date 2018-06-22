f2c741a5d4c77716b34bf631fdf87c870620c6f0
dlin-talend <dlin@talend.com>
2018-06-22 10:31:47 +0800

    feat(TUP-20277):Deprecated items on 7.1 (#1742)

--- main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/utils/ConvertJobsUtil.java
+++ main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/utils/ConvertJobsUtil.java
@@ -148,7 +148,7 @@ public class ConvertJobsUtil {
     }
 
     public static enum JobStreamingFramework {
-        STORMFRAMEWORK("Storm", "_STORM_STORM_FRAMEWORK_"), //$NON-NLS-1$ //$NON-NLS-2$
+        STORMFRAMEWORK("Storm (Deprecated)", "_STORM_STORM_FRAMEWORK_"), //$NON-NLS-1$ //$NON-NLS-2$
         SPARKSTREAMINGFRAMEWORK("Spark Streaming", "_STORM_SPARKSTREAMING_FRAMEWORK_"); //$NON-NLS-1$ //$NON-NLS-2$
 
         private String displayName;
