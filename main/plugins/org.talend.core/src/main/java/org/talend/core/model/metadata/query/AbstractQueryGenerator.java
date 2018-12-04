850eda7f611eafb12f2c7fefa9ad2afce664326a
jiezhang-tlnd <40561780+jiezhang-tlnd@users.noreply.github.com>
2018-12-04 09:37:07 +0800

    fix(TUP-20244)Guess query is not correct for tHiveInput if hivetable name is used a context value (#2061) (#2067)
    * fix(TUP-20244)Guess query is not correct for tHiveInput if hivetable
    name is used a context value
    
    * fix(TUP-20244)Guess query is not correct for tHiveInput if hivetable
    name is used a context value

--- main/plugins/org.talend.core/src/main/java/org/talend/core/model/metadata/query/AbstractQueryGenerator.java
+++ main/plugins/org.talend.core/src/main/java/org/talend/core/model/metadata/query/AbstractQueryGenerator.java
@@ -417,7 +417,9 @@ public abstract class AbstractQueryGenerator implements IQueryGenerator {
             final String tableNameWithDBAndSchema = getTableNameWithDBAndSchema(dbName, schemaName, tableName);
             String columnField = generateColumnFields(tableNameWithDBAndSchema);
             if (dbType == EDatabaseTypeName.HIVE) {
-                columnField = generateColumnFields(tableName);
+                columnField = ContextParameterUtils.containContextVariables(tableName)
+                        ? generateColumnFields(SQL_START_CONNECTION + tableName + SQL_END_CONNECTION)
+                        : generateColumnFields(tableName);
             }
 
             StringBuffer sql = new StringBuffer(100);
