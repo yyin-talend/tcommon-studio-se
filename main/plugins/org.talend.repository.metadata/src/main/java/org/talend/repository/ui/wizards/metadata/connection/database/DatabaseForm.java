fa1a323d8e16264882ec9acc159da55ce5c81682
jiezhang-tlnd <40561780+jiezhang-tlnd@users.noreply.github.com>
2018-12-11 16:46:21 +0800

    fix(TUP-21074)Remove HDB properties in SAP HANA components (#2080)
    * fix(TUP-21074)Remove HDB properties in SAP HANA components
    
    * fix(TUP-21074) remove the method of asSAPHanaVersionEnable

--- main/plugins/org.talend.repository.metadata/src/main/java/org/talend/repository/ui/wizards/metadata/connection/database/DatabaseForm.java
+++ main/plugins/org.talend.repository.metadata/src/main/java/org/talend/repository/ui/wizards/metadata/connection/database/DatabaseForm.java
@@ -5247,7 +5247,6 @@ public class DatabaseForm extends AbstractForm {
         boolean isMySQL = asMySQLVersionEnable();
         boolean isVertica = asVerticaVersionEnable();
         boolean isSAS = asSASVersionEnable();
-        boolean isSAPHana = asSAPHanaVersionEnable();
         boolean isImpala = ImpalaVersionEnable();
         boolean isMsSQL = asMsSQLVersionEnable();
         boolean isSybase = asSybaseVersionEnable();
@@ -5292,8 +5291,7 @@ public class DatabaseForm extends AbstractForm {
             dbVersionCombo.getCombo().setItems(versions);
             dbVersionCombo.setHideWidgets(false);
         } else if (dbType.equals(EDatabaseConnTemplate.SAPHana.getDBDisplayName())) {
-            dbVersionCombo.getCombo().setItems(versions);
-            dbVersionCombo.setHideWidgets(!isSAPHana);
+            dbVersionCombo.setHideWidgets(true);
         }else if (dbType.equals(EDatabaseConnTemplate.SYBASEASE_16_SA.getDBDisplayName())) {
             dbVersionCombo.getCombo().setItems(versions);
             dbVersionCombo.setHideWidgets(!isSybase);
@@ -6870,21 +6868,6 @@ public class DatabaseForm extends AbstractForm {
         }
         EDatabaseConnTemplate template = EDatabaseConnTemplate.indexOfTemplate(getConnectionDBType());
         return template != null && template == EDatabaseConnTemplate.SAS
-                && LanguageManager.getCurrentLanguage().equals(ECodeLanguage.JAVA);
-    }
-
-    /**
-     * 
-     * DOC hwang Comment method "asSAPHanaVersionEnable".
-     * 
-     * @return
-     */
-    private boolean asSAPHanaVersionEnable() {
-        if (getConnectionDBType().length() <= 0) {
-            return false;
-        }
-        EDatabaseConnTemplate template = EDatabaseConnTemplate.indexOfTemplate(getConnectionDBType());
-        return template != null && template == EDatabaseConnTemplate.SAPHana
                 && LanguageManager.getCurrentLanguage().equals(ECodeLanguage.JAVA);
     }
 
