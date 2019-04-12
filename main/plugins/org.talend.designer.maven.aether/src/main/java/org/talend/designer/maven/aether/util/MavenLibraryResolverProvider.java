9aa2c69eb4bdf1ba0611fa455f1e83918a304dd1
kjwang-talend <kjwang@talend.com>
2019-04-12 11:01:49 +0800

    Kjwang/feat tup 21189 remove jar license (#2223)
    * TUP-21189 Remove jar license fetch from studio startup
    https://jira.talendforge.org/browse/TUP-21189
    
    * TUP-21189 Remove jar license fetch from studio startup
    https://jira.talendforge.org/browse/TUP-21189
    
    * TUP-21189 Improve fetch license feature that can retrieve by
    CI/commandline
    https://jira.talendforge.org/browse/TUP-21866
    
    * TUP-21189 Improve fetch license feature that can retrieve by
    CI/commandline
    https://jira.talendforge.org/browse/TUP-21866
    
    * Fix: TUP-21866 Remove jar license fetch from studio startup
    https://jira.talendforge.org/browse/TUP-21866

---
+++ main/plugins/org.talend.designer.maven.aether/src/main/java/org/talend/designer/maven/aether/util/MavenLibraryResolverProvider.java
@@ -0,0 +1,165 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.designer.maven.aether.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.eclipse.aether.DefaultRepositorySystemSession;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
+import org.eclipse.aether.impl.DefaultServiceLocator;
+import org.eclipse.aether.repository.Authentication;
+import org.eclipse.aether.repository.LocalRepository;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.ArtifactDescriptorRequest;
+import org.eclipse.aether.resolution.ArtifactDescriptorResult;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResult;
+import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
+import org.eclipse.aether.spi.connector.transport.TransporterFactory;
+import org.eclipse.aether.transport.file.FileTransporterFactory;
+import org.eclipse.aether.transport.http.HttpTransporterFactory;
+import org.eclipse.aether.util.repository.AuthenticationBuilder;
+import org.eclipse.m2e.core.MavenPlugin;
+import org.talend.core.nexus.ArtifactRepositoryBean;
+import org.talend.core.nexus.TalendLibsServerManager;
+import org.talend.core.runtime.maven.MavenArtifact;
+
+public class MavenLibraryResolverProvider {
+    
+    public static final String KEY_LOCAL_MVN_REPOSITORY = "talend.mvn.repository";
+
+    private static Map<String, RemoteRepository> urlToRepositoryMap = new HashMap<String, RemoteRepository>();
+
+    private RepositorySystem defaultRepoSystem;
+
+    private RepositorySystemSession defaultRepoSystemSession;
+
+    private RemoteRepository defaultRemoteRepository = null;
+
+    private static MavenLibraryResolverProvider instance;
+
+    public static MavenLibraryResolverProvider getInstance() {
+        if (instance == null) {
+            synchronized (MavenLibraryResolverProvider.class) {
+                if (instance == null) {
+                    try {
+                        instance = new MavenLibraryResolverProvider();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+
+                }
+            }
+        }
+        return instance;
+    }
+
+    private MavenLibraryResolverProvider() {
+        defaultRepoSystem = newRepositorySystem();
+        defaultRepoSystemSession = newSession(defaultRepoSystem, getLocalMVNRepository());
+        ArtifactRepositoryBean talendServer = TalendLibsServerManager.getInstance().getTalentArtifactServer();
+        if (talendServer.getUserName() == null && talendServer.getPassword() == null) {
+            defaultRemoteRepository = new RemoteRepository.Builder("talend", "default", talendServer.getRepositoryURL()).build();
+        } else {
+            Authentication authentication = new AuthenticationBuilder().addUsername(talendServer.getUserName())
+                    .addPassword(talendServer.getPassword()).build();
+            defaultRemoteRepository = new RemoteRepository.Builder("talend", "default", talendServer.getRepositoryURL())
+                    .setAuthentication(authentication).build();
+        }
+    }
+
+    public ArtifactResult resolveArtifact(MavenArtifact aritfact) throws Exception {
+        RemoteRepository remoteRepo = getRemoteRepositroy(aritfact);
+        Artifact artifact = new DefaultArtifact(aritfact.getGroupId(), aritfact.getArtifactId(), aritfact.getClassifier(),
+                aritfact.getType(), aritfact.getVersion());
+        ArtifactRequest artifactRequest = new ArtifactRequest();
+        artifactRequest.addRepository(remoteRepo);
+        artifactRequest.setArtifact(artifact);
+        ArtifactResult result = defaultRepoSystem.resolveArtifact(defaultRepoSystemSession, artifactRequest);
+        return result;
+    }
+
+    public Map<String, Object> resolveDescProperties(MavenArtifact aritfact) throws Exception {
+        RemoteRepository remoteRepo = getRemoteRepositroy(aritfact);
+        Artifact artifact = new DefaultArtifact(aritfact.getGroupId(), aritfact.getArtifactId(), aritfact.getClassifier(),
+                aritfact.getType(), aritfact.getVersion());
+        ArtifactDescriptorRequest artifactRequest = new ArtifactDescriptorRequest();
+        artifactRequest.addRepository(remoteRepo);
+        artifactRequest.setArtifact(artifact);
+        ArtifactDescriptorResult result = defaultRepoSystem.readArtifactDescriptor(defaultRepoSystemSession, artifactRequest);
+        return result.getProperties();
+    }
+
+    public RemoteRepository getRemoteRepositroy(MavenArtifact aritfact) {
+        if (aritfact != null && aritfact.getRepositoryUrl() != null) {
+            if (urlToRepositoryMap.containsKey(aritfact.getRepositoryUrl())) {
+                return urlToRepositoryMap.get(aritfact.getRepositoryUrl());
+            }
+
+            RemoteRepository repository = buildRemoteRepository(aritfact);
+            urlToRepositoryMap.put(aritfact.getRepositoryUrl(), repository);
+            return repository;
+        }
+        return defaultRemoteRepository;
+    }
+
+    private RemoteRepository buildRemoteRepository(MavenArtifact aritfact) {
+        RemoteRepository repository = null;
+        if (aritfact.getUsername() == null && aritfact.getPassword() == null) {
+            repository = new RemoteRepository.Builder("talend", "default", aritfact.getRepositoryUrl()).build();
+        } else {
+            Authentication authentication = new AuthenticationBuilder().addUsername(aritfact.getUsername())
+                    .addPassword(aritfact.getPassword()).build();
+            repository = new RemoteRepository.Builder("talend", "default", aritfact.getRepositoryUrl())
+                    .setAuthentication(authentication).build();
+        }
+        return repository;
+    }
+
+    private RepositorySystem newRepositorySystem() {
+        DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
+        locator.addService(RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class);
+        locator.addService(TransporterFactory.class, FileTransporterFactory.class);
+        locator.addService(TransporterFactory.class, HttpTransporterFactory.class);
+
+        return locator.getService(RepositorySystem.class);
+    }
+
+    private RepositorySystemSession newSession(RepositorySystem system, String target) {
+        DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
+
+        LocalRepository localRepo = new LocalRepository( /* "target/local-repo" */target);
+        session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localRepo));
+
+        return session;
+    }
+    
+    private String getLocalMVNRepository() {
+        String repository = null;
+        try {
+            repository = MavenPlugin.getMaven().getLocalRepositoryPath();
+        } catch (Exception ex) {
+            // Ignore here
+        }
+        if (repository == null) {
+            repository = System.getProperty(KEY_LOCAL_MVN_REPOSITORY);
+        }
+        return repository;
+    }
+}
---
+++ main/plugins/org.talend.librariesmanager/src/main/java/org/talend/librariesmanager/librarydata/LibraryDataService.java
@@ -0,0 +1,301 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.librariesmanager.librarydata;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.Platform;
+import org.talend.core.runtime.maven.MavenArtifact;
+import org.talend.core.runtime.maven.MavenUrlHelper;
+import org.talend.designer.maven.aether.util.MavenLibraryResolverProvider;
+
+public class LibraryDataService {
+
+    private static Logger logger = Logger.getLogger(LibraryDataService.class);
+
+    /**
+     * {@value}
+     * <p>
+     * System property of build studio library license file, the default value is <b>false</b>.
+     */
+    public static final String KEY_LIBRARIES_BUILD_LICENSE = "talend.libraries.buildLicense"; //$NON-NLS-1$
+
+    /**
+     * {@value}
+     * <p>
+     * System property of build studio library file, the default value is <b>false</b>.
+     */
+    public static final String KEY_LIBRARIES_BUILD_JAR = "talend.libraries.buildJar"; //$NON-NLS-1$
+
+    /**
+     * {@value}
+     * <p>
+     * System property of studio library data file folder path, the default value is Studio install
+     * folder/configuration.
+     */
+    public static final String KEY_LIBRARIES_DATA_FOLDER = "talend.libraries.folder"; //$NON-NLS-1$
+
+    /**
+     * {@value}
+     * <p>
+     * System property of retrieve library data, the default value is <b>true</b>.
+     */
+    public static final String KEY_BUILD_LIBRARY_IF_MISSING = "talend.libraries.buildIfMissing"; //$NON-NLS-1$
+
+    /**
+     * {@value}
+     * <p>
+     * System property of retrieve library data, the default value is <b>false</b>.
+     */
+    public static final String KEY_BUILD_LIBRARY_IF_LICENSE_MISSING = "talend.libraries.buildIfLicenseMissing"; //$NON-NLS-1$
+
+    private static final String LIBRARIES_DATA_FILE_NAME = "library_data.index"; //$NON-NLS-1$
+
+    private static final String UNRESOLVED_LICENSE_NAME = "UNKNOW"; //$NON-NLS-1$
+
+    private static boolean buildLibraryIfFileMissing = true;
+
+    private boolean buildLibraryLicense = false;
+
+    private boolean buildLibraryIfLibraryMissing = true;
+
+    private boolean buildLibraryIfLicenseMissing = false;
+
+    private boolean buildLibraryJarFile = false;
+
+    private static final Map<String, Library> mvnToLibraryMap = new ConcurrentHashMap<String, Library>();
+
+    private static LibraryDataService instance;
+
+    private LibraryDataJsonProvider dataProvider;
+
+    private Set<String> retievedMissingSet = new HashSet<String>();
+
+    private int repeatTime = 3;
+
+    private LibraryLicense unknownLicense;;
+
+    private LibraryDataService() {
+        buildLibraryLicense = Boolean.valueOf(System.getProperty(KEY_LIBRARIES_BUILD_LICENSE, Boolean.FALSE.toString()));
+        buildLibraryIfLibraryMissing = Boolean.valueOf(System.getProperty(KEY_BUILD_LIBRARY_IF_MISSING, Boolean.TRUE.toString()));
+        buildLibraryIfLicenseMissing = Boolean
+                .valueOf(System.getProperty(KEY_BUILD_LIBRARY_IF_LICENSE_MISSING, Boolean.FALSE.toString()));
+        buildLibraryJarFile = Boolean.valueOf(System.getProperty(KEY_LIBRARIES_BUILD_JAR, Boolean.FALSE.toString()));
+        File libraryDataFile = getLibraryDataFile();
+        if (buildLibraryLicense) {
+            if (libraryDataFile.exists()) {
+                libraryDataFile.delete();
+            }
+        }
+        unknownLicense = new LibraryLicense();
+        unknownLicense.setName(UNRESOLVED_LICENSE_NAME);
+        dataProvider = new LibraryDataJsonProvider(libraryDataFile);
+        mvnToLibraryMap.putAll(dataProvider.loadLicenseData());
+    }
+
+    public static LibraryDataService getInstance() {
+        if (instance == null) {
+            synchronized (LibraryDataService.class) {
+                if (instance == null) {
+                    instance = new LibraryDataService();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void buildLibraryLicenseData(Set<String> mvnUrlList) {
+        for (String mvnUrl : mvnUrlList) {
+            Library libraryObj = resolve(mvnUrl);
+            mvnToLibraryMap.put(getShortMvnUrl(mvnUrl), libraryObj);
+
+        }
+        dataProvider.saveLicenseData(mvnToLibraryMap);
+    }
+
+    private Library resolve(String mvnUrl) {
+        MavenArtifact artifact = MavenUrlHelper.parseMvnUrl(mvnUrl);
+        Library libraryObj = new Library();
+        libraryObj.setGroupId(artifact.getGroupId());
+        libraryObj.setArtifactId(artifact.getArtifactId());
+        libraryObj.setVersion(artifact.getVersion());
+        libraryObj.setMvnUrl(getShortMvnUrl(mvnUrl));
+        libraryObj.setType(artifact.getType());
+        libraryObj.setClassifier(artifact.getClassifier());
+        boolean hasError = false;
+        logger.debug("Resolving artifact descriptor:" + getShortMvnUrl(mvnUrl)); //$NON-NLS-1$
+        try {
+            for (int repeated = 0; repeated < repeatTime; repeated++) {
+                try {
+                    if (repeated > 0) {
+                        Thread.sleep(1000); // To avoid server connection pool shut down
+                    }
+                    Map<String, Object> properties = MavenLibraryResolverProvider.getInstance().resolveDescProperties(artifact);
+                    parseDescriptorResult(libraryObj, properties);
+                    hasError = false;
+                } catch (Exception e) {
+                    hasError = true;
+                }
+                if (!hasError) {
+                    break;
+                }
+            }
+        } finally {
+            if (libraryObj.getLicenses().size() == 0) {
+                libraryObj.setLicenseMissing(true);
+                libraryObj.getLicenses().add(unknownLicense);
+            }
+        }
+        logger.debug("Resolved artifact descriptor:" + getShortMvnUrl(mvnUrl)); //$NON-NLS-1$
+        if (buildLibraryJarFile) {
+            boolean jarMissing = false;
+            try {
+                MavenLibraryResolverProvider.getInstance().resolveArtifact(artifact);
+            } catch (Exception ex) {
+                jarMissing = true;
+                libraryObj.setJarMissing(jarMissing);
+            }
+        }
+
+        return libraryObj;
+    }
+
+    private void parseDescriptorResult(Library libraryObj, Map<String, Object> properties) {
+        int licenseCount = 0;
+        if (properties.containsKey("license.count")) { //$NON-NLS-1$
+            licenseCount = (int) properties.get("license.count"); //$NON-NLS-1$
+        }
+        for (int i = 0; i < licenseCount; i++) {
+            String name = getStringValue(properties.get("license." + i + ".name")); //$NON-NLS-1$ //$NON-NLS-2$
+            String url = getStringValue(properties.get("license." + i + ".url")); //$NON-NLS-1$ //$NON-NLS-2$
+            String comments = getStringValue(properties.get("license." + i + ".comments")); //$NON-NLS-1$ //$NON-NLS-2$
+            String distribution = getStringValue(properties.get("license." + i + ".distribution")); //$NON-NLS-1$ //$NON-NLS-2$
+
+            LibraryLicense license = new LibraryLicense();
+            license.setName(name);
+            license.setUrl(url);
+            license.setComments(comments);
+            license.setDistribution(distribution);
+            libraryObj.getLicenses().add(license);
+        }
+    }
+
+    private String getStringValue(Object value) {
+        if (value == null) {
+            return null;
+        }
+        return value.toString();
+    }
+
+    public boolean isBuildLibrariesData() {
+        if (buildLibraryLicense) {
+            return true;
+        }
+        if (buildLibraryIfFileMissing && !getLibraryDataFile().exists()) {
+            return true;
+        }
+        return false;
+    }
+
+    public File getLibraryDataFile() {
+        String folder = System.getProperty(KEY_LIBRARIES_DATA_FOLDER);
+        if (folder == null) {
+            folder = new File(Platform.getInstallLocation().getURL().getPath(), "configuration").getAbsolutePath(); //$NON-NLS-1$
+        }
+        return new File(folder, LIBRARIES_DATA_FILE_NAME);
+    }
+
+    public void fillLibraryData(String mvnUrl, MavenArtifact artifact) {
+        String shortMvnUrl = getShortMvnUrl(mvnUrl);
+        Library object = mvnToLibraryMap.get(shortMvnUrl);
+        if (!retievedMissingSet.contains(mvnUrl) && ((object == null && buildLibraryIfLibraryMissing)
+                || (object.isLicenseMissing() && buildLibraryIfLicenseMissing))) {
+            Library newObject = null;
+            System.out.println(mvnUrl);
+            retievedMissingSet.add(mvnUrl);
+            try {
+                newObject = resolve(mvnUrl);
+            } catch (Exception e) {
+                logger.warn("Resolve pom failed:" + shortMvnUrl); //$NON-NLS-1$
+            }
+            if (newObject != null && (evaluateLibrary(newObject) > evaluateLibrary(object))) {
+                object = newObject;
+                mvnToLibraryMap.put(shortMvnUrl, object);
+                dataProvider.saveLicenseData(mvnToLibraryMap);
+            }
+        }
+        if (object != null) {
+            List<LibraryLicense> objLicenseList = object.getLicenses();
+            LibraryLicense bestLicense = null;
+            if (objLicenseList.size() >= 1) {
+                bestLicense = objLicenseList.get(0);
+            }
+            if (bestLicense != null) {
+                artifact.setLicense(bestLicense.getName());
+                artifact.setLicenseUrl(bestLicense.getUrl());
+            }
+            // Artifact id
+            if (StringUtils.isEmpty(artifact.getArtifactId()) && StringUtils.isNotEmpty(object.getArtifactId())) {
+                artifact.setArtifactId(object.getArtifactId());
+            }
+
+            // Group id
+            if (StringUtils.isEmpty(artifact.getGroupId()) && StringUtils.isNotEmpty(object.getGroupId())) {
+                artifact.setGroupId(object.getGroupId());
+            }
+
+            // Version
+            if (StringUtils.isEmpty(artifact.getVersion()) && StringUtils.isNotEmpty(object.getVersion())) {
+                artifact.setVersion(object.getVersion());
+            }
+
+            // Type
+            if (StringUtils.isEmpty(artifact.getType()) && StringUtils.isNotEmpty(object.getType())) {
+                artifact.setType(object.getType());
+            }
+
+            // URL
+            if (StringUtils.isEmpty(artifact.getUrl()) && StringUtils.isNotEmpty(object.getUrl())) {
+                artifact.setUrl(object.getUrl());
+            }
+        }
+    }
+
+    private String getShortMvnUrl(String mvnUrl) {
+        if (mvnUrl.indexOf("@") > 0 && mvnUrl.indexOf("!") > 0) {
+            mvnUrl = "mvn:" + mvnUrl.substring(mvnUrl.indexOf("!") + 1);
+        }
+        return mvnUrl;
+    }
+
+    private int evaluateLibrary(Library obj1) {
+        int score = -1;
+        if (obj1 != null) {
+            score++;
+            if (!obj1.isJarMissing()) {
+                score++;
+            }
+            if (!obj1.isLicenseMissing()) {
+                score+=2;
+            }
+        }
+        return score;
+    }
+}
