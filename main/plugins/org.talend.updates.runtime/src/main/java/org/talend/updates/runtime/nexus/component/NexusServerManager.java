f287715a5318a178cc9698302c6aa8ae59f32459
Zhiwei Xue <zwxue@talend.com>
2019-06-06 18:46:00 +0800

    fix(TUP-23222):support nexus3/artifactory for properties server setup (#2479)

--- main/plugins/org.talend.updates.runtime/src/main/java/org/talend/updates/runtime/nexus/component/NexusServerManager.java
+++ main/plugins/org.talend.updates.runtime/src/main/java/org/talend/updates/runtime/nexus/component/NexusServerManager.java
@@ -12,6 +12,7 @@
 // ============================================================================
 package org.talend.updates.runtime.nexus.component;
 
+import org.apache.commons.lang3.StringUtils;
 import org.talend.commons.exception.ExceptionHandler;
 import org.talend.commons.exception.PersistenceException;
 import org.talend.core.GlobalServiceRegister;
@@ -19,6 +20,7 @@ import org.talend.core.context.Context;
 import org.talend.core.context.RepositoryContext;
 import org.talend.core.model.general.INexusService;
 import org.talend.core.nexus.ArtifactRepositoryBean;
+import org.talend.core.nexus.ArtifactRepositoryBean.NexusType;
 import org.talend.core.runtime.CoreRuntimePlugin;
 import org.talend.core.runtime.projectsetting.ProjectPreferenceManager;
 import org.talend.repository.model.IProxyRepositoryFactory;
@@ -40,6 +42,8 @@ public class NexusServerManager {
     public static final String PROP_KEY_NEXUS_PASS = "components.nexus.pass"; //$NON-NLS-1$
 
     private static final String DEFAULT_REPOSITORY_ID = "releases"; //$NON-NLS-1$
+
+    private static final String SLASH = "/"; //$NON-NLS-1$
 
     private static NexusServerManager instance;
 
@@ -79,6 +83,20 @@ public class NexusServerManager {
         serverBean.setRepositoryId(repoId);
         serverBean.setUserName(nexusUser);
         serverBean.setPassword(nexusPass);
+        
+        String repType = StringUtils.stripEnd(nexusUrl, SLASH);
+        repType = StringUtils.substringAfterLast(repType, SLASH).toLowerCase();
+        NexusType type = null;
+        if (repType.equals("nexus")) { //$NON-NLS-1$
+            type = NexusType.NEXUS_2;
+        } else if (repType.equals("artifactory")) { //$NON-NLS-1$
+            type = NexusType.ARTIFACTORY;
+        } else {
+            type = NexusType.NEXUS_3;
+        }
+        if (type != null) {
+            serverBean.setType(type.name());
+        }
         return serverBean;
     }
 
