68372a9d7a41890d56635b1573766fbec9a71d31
hwang-talend <hwang@talend.com>
2020-02-21 15:25:05 +0800

    bugfix(TUP-25959):Incorrect message is observed in the tWarn component (#3098)
    bugfix(TUP-25959):Incorrect message is observed in the tWarn component

---
+++ main/plugins/org.talend.core/src/main/java/org/talend/core/model/components/conversions/RenameETLJDBCComponentConversion.java
@@ -0,0 +1,40 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.model.components.conversions;
+
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+public class RenameETLJDBCComponentConversion extends RenameComponentConversion{
+
+    private String newName;
+    
+    private String oldName;
+
+    public RenameETLJDBCComponentConversion(String newName, String oldName) {
+        super(newName);
+        this.newName = newName;
+        this.oldName = oldName;
+    }
+
+    public void transform(NodeType node) {
+        node.setComponentName(newName);
+        ProcessType item = (ProcessType) node.eContainer();
+        String oldNodeUniqueName = ComponentUtilities.getNodeUniqueName(node);
+        String newNodeUniqueName = oldNodeUniqueName.replaceAll(oldName, newName);
+        ComponentUtilities.setNodeUniqueName(node, newNodeUniqueName);
+        replaceAllInAllNodesParameterValue(item, oldNodeUniqueName, newNodeUniqueName);
+    }
+
+}
