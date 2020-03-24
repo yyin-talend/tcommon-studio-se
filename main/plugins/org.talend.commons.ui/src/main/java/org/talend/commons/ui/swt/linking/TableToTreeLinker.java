3517cd5e49c49ddd81803aae2895812181df868e
hzhao-talendbj <hzhao@talend.com>
2020-03-24 15:31:15 +0800

    fix column names are same issue

--- main/plugins/org.talend.commons.ui/src/main/java/org/talend/commons/ui/swt/linking/TableToTreeLinker.java
+++ main/plugins/org.talend.commons.ui/src/main/java/org/talend/commons/ui/swt/linking/TableToTreeLinker.java
@@ -25,9 +25,7 @@ import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Item;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Tree;
@@ -106,15 +104,6 @@ public class TableToTreeLinker<D1, D2> extends BgDrawableComposite implements IB
         this.source = sourceTable;
 
         dataToTableItemCache = new DataToTableItemCache(sourceTable);
-        if (Platform.OS_LINUX.equals(Platform.getOS())) {
-            this.source.addListener(SWT.EraseItem, new Listener() {
-
-                @Override
-                public void handleEvent(Event event) {
-                    drawBackground(event.gc);
-                }
-            });
-        }
     }
 
     protected IStyleLink getDefaultStyleLink() {
@@ -242,7 +231,7 @@ public class TableToTreeLinker<D1, D2> extends BgDrawableComposite implements IB
 
             int yStraight = sourceToCommonPoint.y + treeItemHeight / 2 + tableItemBounds.y;
             pointEndStraight.x = sourceToCommonPoint.x + xStartBezierLink;
-            if (Platform.OS_MACOSX.equals(Platform.getOS())) {
+            if (Platform.OS_MACOSX.equals(Platform.getOS()) || Platform.OS_LINUX.equals(Platform.getOS())) {
                 pointStartStraight.x = sourceToCommonPoint.x + tableItem.getParent().getBounds().width;
                 pointEndStraight.x = pointStartStraight.x;
             } else {
