f4461e8391b65cf05215df6f03f92ba4e02f5f71
Zhiwei Xue <zwxue@talend.com>
2020-04-03 11:37:58 +0800

    fix(TUP-26233):update related extensions. (#3179)

--- main/plugins/org.talend.repository.items.importexport/src/main/java/org/talend/repository/items/importexport/handlers/exports/SyncChildrenTestReportsProvider.java
+++ main/plugins/org.talend.repository.items.importexport/src/main/java/org/talend/repository/items/importexport/handlers/exports/SyncChildrenTestReportsProvider.java
@@ -13,14 +13,15 @@
 package org.talend.repository.items.importexport.handlers.exports;
 
 import java.io.File;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.talend.commons.runtime.utils.io.FileCopyUtils;
 import org.talend.core.GlobalServiceRegister;
+import org.talend.core.model.process.JobInfo;
 import org.talend.core.model.properties.ProcessItem;
 import org.talend.core.runtime.process.ITalendProcessJavaProject;
 import org.talend.core.runtime.repository.build.IBuildResourcesProvider;
@@ -31,6 +32,7 @@ public class SyncChildrenTestReportsProvider implements IBuildResourcesProvider 
 
     private static final String TEST_REPORTS_FOLDER = "surefire-reports";
 
+    @SuppressWarnings("unchecked")
     @Override
     public void prepare(IProgressMonitor monitor, Map<String, Object> parameters) throws Exception {
         if (parameters == null) {
@@ -45,7 +47,8 @@ public class SyncChildrenTestReportsProvider implements IBuildResourcesProvider 
         if (processItem == null) {
             return;
         }
-        final List dependenciesItems = (List) ParametersUtil.getObject(parameters, OBJ_ITEM_DEPENDENCIES, List.class);
+        final Set<JobInfo> dependenciesItems = (Set<JobInfo>) ParametersUtil.getObject(parameters, OBJ_ITEM_DEPENDENCIES,
+                Set.class);
         if (dependenciesItems == null || dependenciesItems.isEmpty()) {
             return;
         }
@@ -58,16 +61,17 @@ public class SyncChildrenTestReportsProvider implements IBuildResourcesProvider 
         final File reportTargetFolder = processJavaProject.getTargetFolder().getFolder(TEST_REPORTS_FOLDER).getLocation()
                 .toFile();
 
-        for (Object item : dependenciesItems) {
-            if (item instanceof ProcessItem) {
-                ITalendProcessJavaProject childJavaProject = runProcessService
-                        .getTalendJobJavaProject(((ProcessItem) item).getProperty());
-                if (childJavaProject != null) {
-                    childJavaProject.getTargetFolder().refreshLocal(IResource.DEPTH_INFINITE, monitor);
-                    final IFolder childReportFolder = childJavaProject.getTargetFolder().getFolder(TEST_REPORTS_FOLDER);
-                    if (childReportFolder.exists()) {
-                        FileCopyUtils.syncFolder(childReportFolder.getLocation().toFile(), reportTargetFolder, false);
-                    }
+        for (JobInfo jobInfo : dependenciesItems) {
+            if (jobInfo.isJoblet()) {
+                continue;
+            }
+            ITalendProcessJavaProject childJavaProject = runProcessService
+                    .getTalendJobJavaProject(jobInfo.getProcessItem().getProperty());
+            if (childJavaProject != null) {
+                childJavaProject.getTargetFolder().refreshLocal(IResource.DEPTH_INFINITE, monitor);
+                final IFolder childReportFolder = childJavaProject.getTargetFolder().getFolder(TEST_REPORTS_FOLDER);
+                if (childReportFolder.exists()) {
+                    FileCopyUtils.syncFolder(childReportFolder.getLocation().toFile(), reportTargetFolder, false);
                 }
             }
         }
