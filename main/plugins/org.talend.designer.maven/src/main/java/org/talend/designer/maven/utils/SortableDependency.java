e3f1c84dfde78409b055993eae82e3176a51d2dd
bhe-talendbj <bhe@talend.com>
2020-05-09 16:48:19 +0800

    fix(TUP-26728): backport
    Conflicts:
    	main/plugins/org.talend.core.runtime/src/main/java/org/talend/designer/runprocess/IRunProcessService.java

--- main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/utils/SortableDependency.java
+++ main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/utils/SortableDependency.java
@@ -4,10 +4,62 @@ import org.apache.maven.model.Dependency;
 
 public class SortableDependency extends Dependency implements Comparable<SortableDependency> {
 
+    private static final long serialVersionUID = -6295226523517981508L;
+
+    /**
+     * dependencies in testcase/routines/beans no need to be assembled in final zip.
+     */
+    private boolean isAssemblyOptional;
+
     @Override
     public int compareTo(SortableDependency o) {
 
-        return getArtifactId().compareTo(o.getArtifactId());
+        int compare = getArtifactId().compareTo(o.getArtifactId());
+        if (compare == 0) {
+            return getVersion().compareTo(o.getVersion());
+        }
+        return compare;
     }
 
+    public boolean isAssemblyOptional() {
+        return isAssemblyOptional;
+    }
+
+    public void setAssemblyOptional(boolean isAssemblyOptional) {
+        this.isAssemblyOptional = isAssemblyOptional;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (getGroupId() == null ? 0 : getGroupId().hashCode());
+        result = 31 * result + (getArtifactId() == null ? 0 : getArtifactId().hashCode());
+        result = 31 * result + (getVersion() == null ? 0 : getVersion().hashCode());
+        result = 31 * result + (getType() == null ? 0 : getType().hashCode());
+        result = 31 * result + (getClassifier() == null ? 0 : getClassifier().hashCode());
+        result = 31 * result + (isAssemblyOptional() ? 1231 : 1237);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof SortableDependency)) {
+            return false;
+        }
+        SortableDependency caseobj = (SortableDependency) obj;
+        return (getGroupId() == caseobj.getGroupId() || (getGroupId() != null && getGroupId().equals(caseobj.getGroupId())))
+                && (getArtifactId() == caseobj.getArtifactId()
+                        || (getArtifactId() != null && getArtifactId().equals(caseobj.getArtifactId())))
+                && (getVersion() == caseobj.getVersion() || (getVersion() != null && getVersion().equals(caseobj.getVersion())))
+                && (getType() == caseobj.getType() || (getType() != null && getType().equals(caseobj.getType())))
+                && (getClassifier() == caseobj.getClassifier()
+                        || (getClassifier() != null && getClassifier().equals(caseobj.getClassifier())))
+                && (isAssemblyOptional() == caseobj.isAssemblyOptional());
+    }
 }
