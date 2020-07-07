bcebeb1e99c461eae64770c99520320a0fdde433
sbliu <sbliu@talend.com>
2020-07-07 16:37:53 +0800

    TUP-27416 Validate network, timeout value read from nexus timeout preference configuration .  (#3430)
    * TUP-27416 Validate network, timeout value read from nexus timeout preference configuration .
    * TUP-27416 transmit timeout as a parameter to check nexus network validation.
    * TUP-27416 Combine same get timeout function in different place.
    * TUP-27416 Refactor , extract constant for reuse.

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/nexus/NexusServerUtils.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/nexus/NexusServerUtils.java
@@ -36,10 +36,8 @@ import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.preferences.IEclipsePreferences;
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.talend.commons.exception.ExceptionHandler;
-import org.talend.core.prefs.ITalendCorePrefConstants;
+import org.talend.commons.utils.network.NetworkUtil;
 import org.talend.core.runtime.maven.MavenArtifact;
 
 /**
@@ -48,10 +46,7 @@ import org.talend.core.runtime.maven.MavenArtifact;
  */
 public class NexusServerUtils {
 
-    /**
-     *
-     */
-    public static final String ORG_TALEND_DESIGNER_CORE = "org.talend.designer.core"; //$NON-NLS-1$
+    public static final String ORG_TALEND_DESIGNER_CORE = NetworkUtil.ORG_TALEND_DESIGNER_CORE;
 
     public static final int CONNECTION_OK = 200;
 
@@ -69,9 +64,7 @@ public class NexusServerUtils {
      * @return
      */
     public static int getTimeout() {
-        IEclipsePreferences node = InstanceScope.INSTANCE.getNode(ORG_TALEND_DESIGNER_CORE);
-        int timeout = node.getInt(ITalendCorePrefConstants.NEXUS_TIMEOUT, 20000);
-        return timeout;
+        return NetworkUtil.getNexusTimeout();
     }
 
     public static boolean checkConnectionStatus(String nexusUrl, String repositoryId, final String userName, final String password) {
