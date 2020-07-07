a2a09fa98c8eafcba37291f930de43f7a2bab262
bhe-talendbj <52311292+bhe-talendbj@users.noreply.github.com>
2020-07-07 09:41:22 +0800

    fix(TUP-27586): fix test case failure (#3443)
    * fix(TUP-27586): fix test case failure
    
    * fix(TUP-27586): fix test case failure

--- main/plugins/org.talend.commons.runtime/src/org/talend/commons/utils/resource/UpdatesHelper.java
+++ main/plugins/org.talend.commons.runtime/src/org/talend/commons/utils/resource/UpdatesHelper.java
@@ -340,9 +340,8 @@ public class UpdatesHelper {
         return null;
     }
 
-    public static Properties loadProductProperties() {
+    public static Properties loadProductProperties(File productFile) {
         Properties prop = new Properties();
-        File productFile = getProductFile();
         if (productFile != null && productFile.exists()) {
             try (FileInputStream fis = new FileInputStream(productFile)) {
                 prop.load(fis);
@@ -355,7 +354,8 @@ public class UpdatesHelper {
 
     public static Set<String> getPatchesInstalled() {
         Set<String> installed = new HashSet<String>();
-        Properties props = loadProductProperties();
+        File productFile = getProductFile();
+        Properties props = loadProductProperties(productFile);
         props.forEach((k, v) -> {
             String key = String.valueOf(k);
             if (key.startsWith(PRE_TALEND_PATCH)) {
